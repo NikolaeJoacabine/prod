b4a2cbcd4ff85429279265c3f71600524a81d036
NikolaeJoacabine <nikolaej07@mail.ru>
2025-03-01 13:31:32 +0300

    реализована регистрация

--- app/src/main/java/com/nikol/prod/AuthStateViewModel.kt
+++ app/src/main/java/com/nikol/prod/AuthStateViewModel.kt
@@ -34,26 +34,26 @@ class AuthStateViewModel @Inject constructor(
     private fun checkAuthStatus() {
         viewModelScope.launch {
             _authState.value = AuthState.Loading
-            delay(2000)//для теста
-            _authState.value = AuthState.Authenticated("")
-//            val credentials = getCurrentUserUseCase.invoke()
-//            if (credentials != null) {
-//                when (val result = loginUseCase.invoke(credentials.first, credentials.second)) {
-//                    is RemoteObtainingLoginResult.Success -> {
-//                        _authState.value = AuthState.Authenticated(result.currentToken)
-//                    }
-//                    is RemoteObtainingLoginResult.LoginError -> {
-//                        logoutUseCase.invoke()
-//                        _authState.value = AuthState.Unauthenticated
-//                    }
-//                    is RemoteObtainingLoginResult.NetworkError -> {
-//                        _authState.value = AuthState.Error(result.message)
-//                    }
-//                    else -> {}
-//                }
-//            } else {
-//                _authState.value = AuthState.Unauthenticated
-//            }
+//            delay(2000)//для теста
+//            _authState.value = AuthState.Authenticated("")
+            val credentials = getCurrentUserUseCase.invoke()
+            if (credentials != null) {
+                when (val result = loginUseCase.invoke(credentials.first, credentials.second)) {
+                    is RemoteObtainingLoginResult.Success -> {
+                        _authState.value = AuthState.Authenticated(result.currentToken)
+                    }
+                    is RemoteObtainingLoginResult.LoginError -> {
+                        logoutUseCase.invoke()
+                        _authState.value = AuthState.Unauthenticated
+                    }
+                    is RemoteObtainingLoginResult.NetworkError -> {
+                        _authState.value = AuthState.Error(result.message)
+                    }
+                    else -> {}
+                }
+            } else {
+                _authState.value = AuthState.Unauthenticated
+            }
         }
     }
 
